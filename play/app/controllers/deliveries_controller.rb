0e657bc5dc5e17b4bec17c5298b8381ea3c1d7c2
VladYordanov <vyordanoff@gmail.com>
2015-09-29 10:52:14 -0700

    Deliveries/Prizes/Users/Bets(matches)/UserBets/Admins working

--- play/app/controllers/deliveries_controller.rb
+++ play/app/controllers/deliveries_controller.rb
@@ -1,92 +1,92 @@
 class DeliveriesController < ApplicationController
-  before_action :set_delivery, only: [:show, :edit, :update, :destroy]
+    before_action :set_delivery, only: [:show, :edit, :update, :destroy]
 
-  # GET /deliveries
-  # GET /deliveries.json
-  def index
-    @deliveries = Delivery.all
-  end
+    # GET /deliveries
+    # GET /deliveries.json
+    def index
+        @deliveries = Delivery.all
+    end
 
-  # GET /deliveries/1
-  # GET /deliveries/1.json
-  def show
-  end
+    # GET /deliveries/1
+    # GET /deliveries/1.json
+    def show
+    end
 
-  # GET /deliveries/new
-  def new
-    @delivery = Delivery.new
-    @delivery.user_id = current_user.id
-    @delivery_user_username = current_user.username
-    @prize = Prize.find(params[:prize_id])
-    @delivery.prize_id = @prize.id
-  end
+    # GET /deliveries/new
+    def new
+        @delivery = Delivery.new
+        @delivery.user_id = current_user.id
+        @delivery_user_username = current_user.username
+        @prize = Prize.find(params[:prize_id])
+        @delivery.prize_id = @prize.id
+    end
 
-  # GET /deliveries/1/edit
-  def edit
-  end
+    # GET /deliveries/1/edit
+    def edit
+    end
 
-  # POST /deliveries
-  # POST /deliveries.json
-  def create
-    @delivery = Delivery.new(delivery_params)
-    @delivery.user_id = current_user.id
-    @delivery.prize_id = 
-    @delivery.save
+    # POST /deliveries
+    # POST /deliveries.json
+    def create
+        @delivery = Delivery.new(delivery_params)
+        @delivery.user_id = current_user.id
+        @delivery.prize_id = 
+        @delivery.save
 
-    respond_to do |format|
-      if @delivery.save
-        format.html { redirect_to @delivery, notice: 'Delivery was successfully created.' }
-        format.json { render :show, status: :created, location: @delivery }
-      else
-        format.html { render :new }
-        format.json { render json: @delivery.errors, status: :unprocessable_entity }
+        respond_to do |format|
+        if @delivery.save
+              format.html { redirect_to @delivery, notice: 'Delivery was successfully created.' }
+              format.json { render :show, status: :created, location: @delivery }
+        else
+              format.html { render :new }
+              format.json { render json: @delivery.errors, status: :unprocessable_entity }
+        end
+    end
+    end
+
+    # PATCH/PUT /deliveries/1
+    # PATCH/PUT /deliveries/1.json
+    def update
+      respond_to do |format|
+        if @delivery.update(delivery_params)
+          format.html { redirect_to @delivery, notice: 'Delivery was successfully updated.' }
+          format.json { render :show, status: :ok, location: @delivery }
+        else
+          format.html { render :edit }
+          format.json { render json: @delivery.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /deliveries/1
-  # PATCH/PUT /deliveries/1.json
-  def update
-    respond_to do |format|
-      if @delivery.update(delivery_params)
-        format.html { redirect_to @delivery, notice: 'Delivery was successfully updated.' }
-        format.json { render :show, status: :ok, location: @delivery }
-      else
-        format.html { render :edit }
-        format.json { render json: @delivery.errors, status: :unprocessable_entity }
+    # DELETE /deliveries/1
+    # DELETE /deliveries/1.json
+    def destroy
+      @delivery.destroy
+      respond_to do |format|
+        format.html { redirect_to deliveries_url, notice: 'Delivery was successfully destroyed.' }
+        format.json { head :no_content }
       end
     end
-  end
 
-  # DELETE /deliveries/1
-  # DELETE /deliveries/1.json
-  def destroy
-    @delivery.destroy
-    respond_to do |format|
-      format.html { redirect_to deliveries_url, notice: 'Delivery was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  def buy
-    @prize = Prize.find(params[:id])
-    @user = current_user
-    @prize.in_stock -= 1
-    @prize.save
-    
-    respond_to do |format|
-      format.html { redirect_to prizes_url, notice: 'You have bought this prize'}
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_delivery
-      @delivery = Delivery.find(params[:id])
+    def buy
+      @prize = Prize.find(params[:id])
+      @user = current_user
+      @prize.in_stock -= 1
+      @prize.save
+      
+      respond_to do |format|
+        format.html { redirect_to prizes_url, notice: 'You have bought this prize'}
+      end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def delivery_params
-      params.require(:delivery).permit(:user_id, :street, :state, :country, :zipcode)
-    end
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_delivery
+        @delivery = Delivery.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def delivery_params
+        params.require(:delivery).permit(:user_id, :street, :state, :country, :zipcode)
+      end
 end
--- play/config/routes.rb
+++ play/config/routes.rb
@@ -1,119 +1,119 @@
 Rails.application.routes.draw do
-  resources :deliveries
-  resources :prizes
-  devise_for :admins
-  devise_for :users
-  resources :user_bets
+	resources :deliveries
+	resources :prizes
+	devise_for :admins
+	devise_for :users
+	resources :user_bets
 
 
-  resource :prizes do
-      get '/:id/buy' => 'prizes#buy', :on => :collection, :as => 'buy_prize'
-      get '/:id/delivery_info' => 'prizes#delivery_info', :on => :collection, :as => 'delivery_info'
-  end
-
-  
-  resource :delivery do
-      get '/new/:prize_id' => 'deliveries#new', :on => :collection, :as => 'new_prize_delivery'
-  end
+	resource :prizes do
+		get '/:id/buy' => 'prizes#buy', :on => :collection, :as => 'buy_prize'
+		get '/:id/delivery_info' => 'prizes#delivery_info', :on => :collection, :as => 'delivery_info'
+	end
 
 
-  resources :main do
-    collection do
-      get 'lol_matches', :as => 'lol_matches'
-      get 'csgo_matches', :as => 'csgo_matches'
-      get 'dota2_matches', :as => 'dota2_matches'
-      get 'sc2_matches', :as => 'sc2_matches'
-    end
-  end
+	resource :delivery do
+		get '/new/:prize_id' => 'deliveries#new', :on => :collection, :as => 'new_prize_delivery'
+	end
 
-  resources :bets do
-    collection do
-      get 'current_matches', :as => 'current_matches'
-      get 'finished_matches', :as => 'finished_matches'
-      get 'lol_matches', :as => 'lol_matches'
-      get 'csgo_matches', :as => 'csgo_matches'
-      get 'dota2_matches', :as => 'dota2_matches'
-      get 'sc2_matches', :as => 'sc2_matches'
-    end
-  end
 
-  resources :users do
-    collection do
-      get 'bets', :as => 'my_bets'
-      get 'profile', :as => 'profile'
-    end
-  end
+	resources :main do
+		collection do
+			get 'lol_matches', :as => 'lol_matches'
+			get 'csgo_matches', :as => 'csgo_matches'
+			get 'dota2_matches', :as => 'dota2_matches'
+			get 'sc2_matches', :as => 'sc2_matches'
+		end
+	end
 
-  resources :admin
+	resources :bets do
+		collection do
+			get 'current_matches', :as => 'current_matches'
+			get 'finished_matches', :as => 'finished_matches'
+			get 'lol_matches', :as => 'lol_matches'
+			get 'csgo_matches', :as => 'csgo_matches'
+			get 'dota2_matches', :as => 'dota2_matches'
+			get 'sc2_matches', :as => 'sc2_matches'
+		end
+	end
 
-  resource :user_bets do
-    get "/bet/:id" => 'user_bets#new', :on => :collection, :as => "user_bet"  
-  end
+	resources :users do
+		collection do
+			get 'bets', :as => 'my_bets'
+			get 'profile', :as => 'profile'
+		end
+	end
 
-  resource :bets do
-    get "/finish/:id" => "bets#finish", :on => :collection, :as => "finish_match"
-  end
+	resources :admin
 
-  resources :admin do
-    get "/panel/" => "admin#panel", :on => :collection, :as => "admin_panel"
-  end
+	resource :user_bets do
+		get "/bet/:id" => 'user_bets#new', :on => :collection, :as => "user_bet"  
+	end
 
-  
-  
+	resource :bets do
+		get "/finish/:id" => "bets#finish", :on => :collection, :as => "finish_match"
+	end
 
-  root 'main#index'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+	resources :admin do
+		get "/panel/" => "admin#panel", :on => :collection, :as => "admin_panel"
+	end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+	root 'main#index'
+	# The priority is based upon order of creation: first created -> highest priority.
+	# See how all your routes lay out with "rake routes".
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+	# You can have the root of your site routed with "root"
+	# root 'welcome#index'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+	# Example of regular route:
+	#   get 'products/:id' => 'catalog#view'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+	# Example of named route that can be invoked with purchase_url(id: product.id)
+	#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+	# Example resource route (maps HTTP verbs to controller actions automatically):
+	#   resources :products
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-end
+	# Example resource route with options:
+	#   resources :products do
+	#     member do
+	#       get 'short'
+	#       post 'toggle'
+	#     end
+	#
+	#     collection do
+	#       get 'sold'
+	#     end
+	#   end
+
+	# Example resource route with sub-resources:
+	#   resources :products do
+	#     resources :comments, :sales
+	#     resource :seller
+	#   end
+
+	# Example resource route with more complex sub-resources:
+	#   resources :products do
+	#     resources :comments
+	#     resources :sales do
+	#       get 'recent', on: :collection
+	#     end
+	#   end
+
+	# Example resource route with concerns:
+	#   concern :toggleable do
+	#     post 'toggle'
+	#   end
+	#   resources :posts, concerns: :toggleable
+	#   resources :photos, concerns: :toggleable
+
+	# Example resource route within a namespace:
+	#   namespace :admin do
+	#     # Directs /admin/products/* to Admin::ProductsController
+	#     # (app/controllers/admin/products_controller.rb)
+	#     resources :products
+	#   end
+	end
