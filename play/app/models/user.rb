58017cb5ba06c897f138e1daa7517e76b32d39d0
VladYordanov <vyordanoff@gmail.com>
2016-02-13 03:46:40 -0800

    moved a functin from controller to the model of users

--- play/app/models/user.rb
+++ play/app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
 	has_many :prizes
 	has_many :deliveries
 
+	def calc_experience(end_points, start_points)
+		@max_points_of_level = start_points.to_i
+		return ( ( experience - end_points ) * 100 ) / ( start_points - end_points )
+	end
+
 	#converts /user/id to /user/name
 	def to_param
 		username
