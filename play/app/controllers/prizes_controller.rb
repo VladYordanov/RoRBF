818e50afd0e293652827bda7024362060d53f1d3
VladYordanov <vyordanoff@gmail.com>
2016-02-24 03:26:18 -0800

    fixed prizes page

--- play/app/controllers/prizes_controller.rb
+++ play/app/controllers/prizes_controller.rb
@@ -71,7 +71,9 @@ class PrizesController < ApplicationController
 
     private
     # Use callbacks to share common setup or constraints between actions.
-
+    def set_prize
+        @prize = Prize.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def prize_params
