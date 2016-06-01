cee422d07e960c4278861673b3edb5022682b5fc
VladYordanov <vyordanoff@gmail.com>
2016-05-31 23:06:57 -0700

    Time starting refactoring and fixes | TO: use the method everywhere where the time of the match is used

--- play/app/controllers/application_controller.rb
+++ play/app/controllers/application_controller.rb
@@ -1,16 +1,25 @@
 class ApplicationController < ActionController::Base
-	# Prevent CSRF attacks by raising an exception.
-	# For APIs, you may want to use :null_session instead.
-	protect_from_forgery with: :exception
-	before_action :configure_permitted_parameters, if: :devise_controller?
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  include ActionView::Helpers::DateHelper
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-	def after_sign_in_path_for(admin)
-		root_path
-	end
+  def after_sign_in_path_for(admin)
+    root_path
+  end
 
-	protected
+  helper_method :start_time
 
-	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_up) << :username
-	end
+  def start_time(match_id)
+    match = Bet.find(match_id)
+    time_left = (match.match_starts_at - Time.now.in_time_zone).to_i
+    return 'started' if time_left <= 0 else return distance_of_time_in_words(time_left)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
 end
